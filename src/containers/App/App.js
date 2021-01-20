35282291a118a40af616318645901366ddea180e
Matan <matan6080@gmail.com>
2021-01-20 15:10:30 +0200

    Application Structure

---
+++ src/containers/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className='container'>
+      This is spotify app
+      <p>This is an another app</p>
+      <p>Hello world</p>
+      <section>love this</section>
+    </div>
+  );
+};
