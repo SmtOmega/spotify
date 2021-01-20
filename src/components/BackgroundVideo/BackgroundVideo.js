3faf24ad2d65a6773d5d907ddd71c9a15f38d8e5
Matan <matan6080@gmail.com>
2021-01-20 23:16:51 +0200

    make Backdrop component visible correctly

--- src/components/BackgroundVideo/BackgroundVideo.js
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -1,11 +1,15 @@
 import React from "react";
 import sample from "../../assets/bg.mp4";
+import { Backdrop } from "../UI/Backdrop/Backdrop";
 import "./BackgroundVideo.scss";
 
 export const BackgroundVideo = () => {
   return (
-    <video className='bg__video' autoPlay loop muted>
-      <source src={sample} type='video/mp4' />
-    </video>
+    <div className='bg'>
+      <Backdrop />
+      <video className='bg__video' autoPlay loop muted>
+        <source src={sample} type='video/mp4' />
+      </video>
+    </div>
   );
 };
